12d5b5e52c07016db29f1bb81cd9e9ab6a2ed714
faheemmhmd <parayfaheem1112@gmail.com>
2024-01-01 15:19:53 +0530

    cube

---
+++ pp2.c
@@ -0,0 +1,11 @@
+/*Take a number(n) from user & output its cube(n*n*n).*/
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the no. : ");
+    scanf("%d",&n);
+    printf("The cube of the no. is : %d \n",n*n*n);
+    return 0;
+
+}
